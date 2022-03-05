bf3ed0392226f27572c62825704ea40318bfef35
2711-bharath <2711bharath@gmail.com>
2022-03-06 00:04:56 +0530

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import TuringCup from "./turingcup"
+import "../styles/global.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <TuringCup />
+    </div>
+  )
 }
---
+++ src/pages/turingcup.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function TuringCup() {
+  return (
+    <div className="iframe-container">
+      <iframe src="./turing_cup.pdf" frameborder="0"></iframe>
+    </div>
+  )
+}
